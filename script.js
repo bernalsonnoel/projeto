7b934ee346fa9da2009826807ac7209acf70a3fd
se liga <nbernalson@gmail.com>
2025-12-12 00:52:59 -0300

    Add countdown timer function for Christmas 2025

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ footer {
   margin-top: 50px;
   font-size: 14px;
 }
+function atualizarContagem() {
+  const natal = new Date("December 25, 2025 00:00:00").getTime();
+  const agora = new Date().getTime();
+  const diferenca = natal - agora;
+
+  const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+  const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+  const segundos = Math.floor((diferenca / 1000) % 60);
+
+  document.getElementById("timer").innerHTML =
+    `${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`;
+
+  setTimeout(atualizarContagem, 1000);
+}
+
+atualizarContagem();
